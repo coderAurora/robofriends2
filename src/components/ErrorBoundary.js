8aa452e6838938c583f69da58067a89f7264a2d0
coderAurora <coderauroratm@gmail.com>
2021-06-01 03:49:52 -0400

    updated packages and added boundary error.js

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({ hasError: true })
+    }
+
+
+    render() {
+        const { hasError } = this.state;
+        if (hasError) {
+            return (
+                <h1>Ooops. That is not good</h1>
+            )
+        }
+        return this.props.children;
+    }
+
+}
+
+export default ErrorBoundary;
